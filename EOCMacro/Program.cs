1c0dd33705039f21e909a547803f9a5faed5e0ac
Stephen King <stephen.king87@outlook.com>
2022-05-18 22:14:13 -0700

    push changes

--- EOCMacro/Program.cs
+++ EOCMacro/Program.cs
@@ -55,23 +55,26 @@ public class Program
     }
 
 	public void GenerateMacroFile(IList<string> combos, string name)
-    {
+	{
 
 		var macromodel = new MacroModel();
 
-		var events = GetMacroEvents(combos);
-		macromodel.Events = (List<MacroEvent>)events;
+		bool isFast = true;
+		var fileDirectory = @$"D:\Spell_Combo_Starting_{name.ToUpper()}";
+		var fileName = isFast ? @$"{fileDirectory}_Fast.json" : @$"{fileDirectory}.json";
 
+		var events = GetMacroEvents(combos, isFast);
+		macromodel.Events = (List<MacroEvent>)events;
 
 		string json = JsonConvert.SerializeObject(macromodel, Newtonsoft.Json.Formatting.Indented,
 					  new JsonSerializerSettings
 					  {
 						  NullValueHandling = NullValueHandling.Ignore,
 					  });
-		File.WriteAllText(@$"D:\Spell_Combo_Starting_{name.ToUpper()}.json", json);
+		File.WriteAllText(@$"D:\Spell_Combo_Starting_{name.ToUpper()}_Fast.json", json);
 	}
 
-	public IList<MacroEvent> GetMacroEvents(IList<string> combos) {		
+	public IList<MacroEvent> GetMacroEvents(IList<string> combos, bool isFast = false) {		
 		// init
 		var events = new List<MacroEvent>();
 		var timestamp = 1000;
@@ -79,9 +82,15 @@ public class Program
 		foreach(var combo in combos)
         {
 			//training begin
-			StartTraining(events, ref timestamp);
+			StartTraining(events, ref timestamp, isFast);
 			//start casting spell
-			CastSpell(events, ref timestamp, combo.ToString());
+			if (isFast)
+			{
+				CastSpell(events, ref timestamp, combo.ToString());
+			} else
+            {
+				CastSpellFast(events, ref timestamp, combo.ToString());
+			}
 			//abort
 			AbortTraining(events, ref timestamp);
 		}
@@ -89,7 +98,7 @@ public class Program
 		return events;
 	}
 
-	public void StartTraining(List<MacroEvent> events, ref int timestamp)
+	public void StartTraining(List<MacroEvent> events, ref int timestamp, bool isFast = false)
     {
 		// training challenge
 		PerformClick(events,ref timestamp, 79.23999786376953, 82.0999984741211);
@@ -101,6 +110,11 @@ public class Program
 
 		// cancel auto (can be replaced by key stroke)
 		PerformClick(events, ref timestamp, 16.149999618530273, 91.75);
+		//give a couple of time to ensure all cooldown finished
+		if (isFast)
+		{
+			timestamp += 4000;
+		}
 	}
 
 	public void AbortTraining(List<MacroEvent> events, ref int timestamp)
@@ -134,8 +148,8 @@ public class Program
 		events.Add(new MacroEvent("KeyUp", timestamp, spell));
 	}
 
-	public void CastSpell(List<MacroEvent> events, ref int timestamp,string combo)
-    {
+	public void CastSpell(List<MacroEvent> events, ref int timestamp, string combo)
+	{
 		//todo: add logic to allow direct cast if spell is not at cooldown 
 		int i = 0;
 		var perunit_cooldown = 400;
@@ -143,19 +157,62 @@ public class Program
 		var earthspell = 15 * perunit_cooldown;
 		var firespell = 18 * perunit_cooldown;
 		var waterspell = 24 * perunit_cooldown;
-		foreach(var x in combo)
-        {
-            switch (x)
-            {
+		foreach (var x in combo)
+		{
+			switch (x)
+			{
 				case 'E': timestamp += earthspell; break;
 				case 'A': timestamp += airspell; break;
 				case 'F': timestamp += firespell; break;
 				case 'W': timestamp += waterspell; break;
-            }
+			}
 			if (i == 0) { timestamp -= 4000; }
+
+			PerformCast(events, ref timestamp, x.ToString());
+			i++;
+		}
+	}
+
+	public void CastSpellFast(List<MacroEvent> events, ref int timestamp,string combo)
+    {
+		int i = 0;
+		var perunit_cooldown = 400;
+		var spellElements = InitializeSpellElementModel(perunit_cooldown);
+		foreach (var x in combo)
+        {
+			timestamp += spellElements[x].currentCoolDown;
+			spellElements = DeductOtherSpellCoolDown(perunit_cooldown, spellElements[x].currentCoolDown, combo, spellElements);
+			spellElements[x].currentCoolDown = spellElements[x].initialCoolDown;
+			//if (i == 0) { timestamp -= 4000; }
 			
 			PerformCast(events, ref timestamp, x.ToString());
 			i++;
 		}
+	}
+
+
+	public IDictionary<char, SpellElementModel> DeductOtherSpellCoolDown(int minimum_cooldown, int cooldown_deducted,  string combo, IDictionary<char, SpellElementModel> spellElementModels)
+    {
+		foreach(var x in combo)
+        {
+			spellElementModels[x].currentCoolDown = (spellElementModels[x].currentCoolDown - cooldown_deducted) <= minimum_cooldown ? minimum_cooldown : ((spellElementModels[x].currentCoolDown - cooldown_deducted) + minimum_cooldown);
+		}
+		return spellElementModels;
+    }
+
+	public IDictionary<char,SpellElementModel> InitializeSpellElementModel(int perunit_cooldown)
+    {
+		int airspell = 12 * perunit_cooldown;
+		int earthspell = 15 * perunit_cooldown;
+		int firespell = 18 * perunit_cooldown;
+		int waterspell = 24 * perunit_cooldown;
+		var spellElementModels = new Dictionary<char, SpellElementModel>()
+		{
+			{'E', new SpellElementModel { currentCoolDown = perunit_cooldown, initialCoolDown = earthspell } },
+			{'A', new SpellElementModel { currentCoolDown = perunit_cooldown, initialCoolDown = airspell } },
+			{'F', new SpellElementModel { currentCoolDown = perunit_cooldown, initialCoolDown = firespell } },
+			{'W', new SpellElementModel { currentCoolDown = perunit_cooldown, initialCoolDown = waterspell } },
+		};
+		return spellElementModels;
 	}
 }
---
+++ EOCMacro/SpellElementModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOCMacro
+{
+    public class SpellElementModel
+    {
+        public int initialCoolDown { get; set; }
+        public int currentCoolDown { get; set; }
+    }
+}
